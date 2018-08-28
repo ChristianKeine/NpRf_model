2263b6e4bb3977d7ea3a23052876d9b3caf495fc
ChristianKeine <christian-keine@uiowa.edu>
2018-08-28 10:12:06 -0500

    merged dependent functions into NpRfModel.m

--- NpRfModel.m
+++ NpRfModel.m
@@ -43,6 +43,34 @@ Res.RRP = PF(1);
 Res.replenishment = PF(3);
 Res.facilitation = f;
 
+end
+
+function EPSC_Diff = NpRfModel_Fit(Data,f,P)
+
+EPSC = NpRfModel_FitFun(Data,f,P);
+
+EPSC_Diff = cumsum(EPSC(:))-cumsum(Data(:));
+% EPSC_Diff = EPSC(:)-Data(:);
+
+end
+
+function [EPSC,N,Rep,f] = NpRfModel_FitFun(Data,f,P)
+
+N(1) = P(1);
+p = P(2);
+R = P(3);
+
+EPSC(1) = p*N(1);
+N(2) = N(1)*(1-p+p*R);
+EPSC(2) = p*f*N(2);
+
+Rep(2) = R*p*N(1);
+
+for t=3:numel(Data)
+    N(t) = N(t-1)*(1-p*f-R+p*f*R)+N(1)*R;
+    EPSC(t) = p*f*N(t);
+    Rep(t) = N(t-1)*R*(p*f-1)+N(1)*R;
+end
 
 
-
+end
